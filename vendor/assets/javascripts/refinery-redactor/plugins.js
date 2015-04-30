4d6f96a0a100d0a55a7b300f0e8a9a7354234bef
Josh McArthur <joshua.mcarthur@gmail.com>
2015-04-30 15:20:05 +1200

    Simplify plugin loading

--- vendor/assets/javascripts/refinery-redactor/plugins.js
+++ vendor/assets/javascripts/refinery-redactor/plugins.js
@@ -1,6 +1,1 @@
-//= require redactor-rails/plugins/clips
-//= require redactor-rails/plugins/fontcolor
-//= require redactor-rails/plugins/fontfamily
-//= require redactor-rails/plugins/fontsize
-//= require redactor-rails/plugins/fullscreen
-//= require redactor-rails/plugins/textdirection
+//= require_tree ./plugins/
